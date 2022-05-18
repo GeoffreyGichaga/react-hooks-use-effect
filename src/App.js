d656feb2e5b1b27ac9b464edecf8530738d7dfd6
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-05-18 12:18:22 +0300

    completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from "react";
+import DogPics from "./DogPics";
 
 function App() {
   useEffect(() => {
     console.log("useEffect called");
-  });
+  },[]);
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return (
+    <>
+    <button>Click Me</button>
+    <DogPics/>
+    </>
+  );
 }
 
 export default App;
